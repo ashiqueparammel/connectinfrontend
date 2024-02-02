a5779d2a28a2ca7cf5fffe2b638f24f8f4c256bd
ashiqueparammel <ashiqueparammel558@gmail.com>
2024-02-02 18:02:10 +0530

    updated bugs

--- src/Pages/User/JobView/JobView.jsx
+++ src/Pages/User/JobView/JobView.jsx
@@ -313,20 +313,40 @@ function JobView() {
     }
 
     const ApplyJobHandle = () => {
-        // job axios
 
-        const postData = {
-            profile: UserProfile.id,
-            job_post: job_id,
-            Experience: expriance,
-            currentSalary: currentCtc,
-            ExpectedSalary: ExpetedCtc,
-            description: description,
-            cv_file:UserProfile.cv_file || null
+        let Cv_file = null
+        const formData = new FormData();
+
+        if (UserProfile.cv_file) {
+            fetch(fileUrl)
+                .then((response) => response.blob())
+                .then((blob) => {
+                    const Cv_file = new File([blob], { type: 'application/pdf' });
+                })
+
+            formData.append('cv_file', Cv_file);
+
         }
+        formData.append('profile',UserProfile.id);
+        formData.append('job_post',job_id);
+        formData.append('Experience',expriance);
+        formData.append('currentSalary',currentCtc);
+        formData.append('ExpectedSalary',ExpetedCtc);
+        formData.append('description',description);
 
 
-        console.log('postdata data:', postData);
+
+        // const postData = {
+        //     profile: UserProfile.id,
+        //     job_post: job_id,
+        //     Experience: expriance,
+        //     currentSalary: currentCtc,
+        //     ExpectedSalary: ExpetedCtc,
+        //     description: description,
+        // }
+
+
+        // console.log('postdata data:', postData);
         const validateForm = () => {
             if (expriance === "") {
                 toast.error('Experience should not be empty!');
@@ -345,7 +365,7 @@ function JobView() {
 
         if (validateForm()) {
             try {
-                axios.post(JobApplicationsAdd, postData)
+                axios.post(JobApplicationsAdd, formData)
                     .then((response) => {
                         const responseData = response.data
                         console.log(response, 'respose Apply Job add');
